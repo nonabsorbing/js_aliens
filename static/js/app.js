3c65addfa18a37a7ec9236be61aa9570f61a992c
Tom Harmon <thomas.harmon@gmail.com>
2018-12-01 23:07:50 -0600

    Final commit - fixed filtering issue

--- static/js/app.js
+++ static/js/app.js
@@ -4,53 +4,55 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 
-data.forEach(function(ufoSightings) {
-  // console.log(ufoSightings);
-  var row = tbody.append("tr");
-  Object.entries(ufoSightings).forEach(function([key, value]) {
-    // console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-    var cell = tbody.append("td");
-    cell.text(value);
+
+function fillTable(fillItUp){
+
+  //clear all rows before filtering
+  tbody.html("");
+
+  fillItUp.forEach(function(ufoSightings) {
+
+    // console.log(ufoSightings);
+
+    var row = tbody.append("tr");
+
+    Object.values(ufoSightings).forEach((val) => {
+      console.log(val);
+
+      var cell = row.append("td");
+
+      cell.text(val); 
+    });
   });
-});
+}
+
+
 
 //set up function for when filter is filled in and clicked
 function handleClick(event) {
 
-  //set up submit variable 
-    var submit = d3.selectAll("#submit").on("click", function() {
-    // Prevent the page from refreshing
-      d3.event.preventDefault();
+   // Prevent the page from refreshing
+
+   d3.event.preventDefault();
 
       // // Select the input element and get the raw HTML node
-      var inputElement = d3.select("#datetime");
+      var sightingDate = d3.select("#datetime").property("value");
             
         // // Get the value property of the input element
-      var inputValue = inputElement.property("value");
+      console.log(sightingDate);
 
-      console.log(inputValue);
-
-
-      //   //clear all rows before filtering
-      // tbody.html("")
 
       // // Set up filter variable - based on filtering 
-      // var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+      var filteredData = data.filter(sighting => sighting.datetime === sightingDate);
 
-      //   filteredData.forEach(function(filterSightings) {
-      //     console.log(filterSightings);
-      //     var row = tbody.append("tr");
-      //     Object.values(filterSightings).forEach(function([key, value]) {
-      //       console.log(key, value);
+          console.log(filteredData);
+         
 
-      //     var cell = tbody.append("td");
-      //     cell.text(value);
-});
+    fillTable(filteredData); 
 }
 
 
 // //update table data
- tbody.on("click", handleClick);
+ d3.selectAll("#filter-btn").on("click", handleClick);
 
+ fillTable(data); 
